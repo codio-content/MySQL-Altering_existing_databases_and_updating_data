92641becc98557969409e34d8255b71bcef811c0
netpoe <yo@soygus.com>
2015-09-14 04:19:17 +0000

    Init sqltests dir

--- uberr.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE trips (
-	id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	trip_datetime_start DATETIME NOT NULL,
-  trip_datetime_end DATETIME,
-	trip_total_fare FLOAT(8,2) DEFAULT 00.00
-) auto_increment = 1;
